e25a0934e2e2c4ff3b3345dbdff0e45900c28ba6
Hassan <hassanch5209@gmail.com>
2023-08-08 01:51:51 +0500

    Wrote basic functionality of presale contract

---
+++ contracts/Presale.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Presale is Pausable, Ownable {
+    
+    bool public _looslyCoupled;
+    IERC20 public _tokenContract;
+    address public _tokenHolder;
+
+    uint256 public _totalTokensForPreSale;
+    uint public _soldTokens;
+    uint256 public _preSalePrice;
+    
+    uint256 public _preSaleStartDate;
+    uint256 public _preSaleEndDate;
+    uint public _cliffPeriod;
+
+    uint256 public _minInvestment;
+    uint256 public _maxInvestment;
+
+    mapping(address => uint256) public _investments;
+    mapping(address => uint256) public _tokensPurchased;
+
+    ////////////////////////////////////////////
+
+    uint256 public _firstReleasePercent;
+    uint256 public _releasePercent;
+    
+    uint256 public _releasePeriod;
+
+    mapping(address => uint256) public _releasedTokens;
+    mapping(address => uint256) public _lastTimeTokenReleased;
+
+    constructor(
+        address erc20Addr,
+        address tokenHolder
+    ) {
+        _tokenContract = IERC20(erc20Addr);
+        _tokenHolder = tokenHolder;
+    }
+
+    function buyTokenInCrypto() public payable {
+        require(block.timestamp >= _preSaleStartDate, "Presale: Presale is not started");
+        require(block.timestamp <= _preSaleEndDate, "Presale: Presale is ended");
+        require(msg.value >= _minInvestment && msg.value <= _maxInvestment, "Presale: Insufficient amount sent");
+
+        uint tokenEarned = msg.value / _preSalePrice;
+        uint price = tokenEarned * _preSalePrice;
+        uint remainingPrice = msg.value - price;
+
+        require(tokenEarned + _soldTokens <= _totalTokensForPreSale, "Presale: Don't have enough available tokens");
+
+        _investments[msg.sender] += price;
+        _tokensPurchased[msg.sender] += tokenEarned;
+        _soldTokens += tokenEarned;
+
+        if (remainingPrice > 0){
+            payable(msg.sender).transfer(remainingPrice);
+        }
+    }
+
+    function buyTokenInFiat(uint tokenToBuy) public onlyOwner{
+        require(block.timestamp >= _preSaleStartDate, "Presale: Presale is not started");
+        require(block.timestamp <= _preSaleEndDate, "Presale: Presale is ended");
+        
+        require(tokenToBuy + _soldTokens <= _totalTokensForPreSale, "Presale: Don't have enough available tokens");
+
+        _tokensPurchased[msg.sender] += tokenToBuy;
+        _soldTokens += tokenToBuy;
+    }
+
+    // user centric release tokens || owner based release tokens
+    function releaseTokens() public {
+        uint tokensToRelease;
+        uint currentTimestamp = block.timestamp;
+
+        require(_tokensPurchased[msg.sender] > 0, "Presale: Don't have any bought token");
+        require(currentTimestamp >= _preSaleEndDate + _cliffPeriod, "Presale: Distribution is not started yet");
+
+        if (_lastTimeTokenReleased[msg.sender] == 0){
+            _lastTimeTokenReleased[msg.sender] =  _preSaleEndDate + _cliffPeriod + 1;
+            tokensToRelease = findValueFromPercentage(_firstReleasePercent, _tokensPurchased[msg.sender]);
+        }
+        else {
+            require(currentTimestamp >= _lastTimeTokenReleased[msg.sender] + _releasePeriod, "Presale: Vesting period is not over");
+            
+            _lastTimeTokenReleased[msg.sender] = _lastTimeTokenReleased[msg.sender] + _releasePeriod;
+            tokensToRelease = findValueFromPercentage(_releasePercent, _tokensPurchased[msg.sender]);
+        }
+
+        uint availableForRelease = _tokensPurchased[msg.sender] - _releasedTokens[msg.sender];
+
+        require(tokensToRelease <= availableForRelease, "Presale: Cannot release tokens more than purchased tokens");
+            
+        _releasedTokens[msg.sender] += tokensToRelease;
+    }
+
+    function claimToken() public {
+        require(_releasedTokens[msg.sender] > 0, "Presale: Don't have any released token");
+
+        if (_looslyCoupled){
+            _tokenContract.transferFrom(_tokenHolder, msg.sender, _releasedTokens[msg.sender]);
+        }
+        else{
+            bytes memory payload = abi.encodeWithSignature("mint(address,uint256)", msg.sender, _releasedTokens[msg.sender]);
+
+            (bool success, ) = _tokenHolder.call(payload);
+            
+           require(success, "Presale: Function call failed");
+        }
+    }
+
+    function updateTotalTokenForPresale(uint updatedSupply) public onlyOwner{
+        _totalTokensForPreSale = updatedSupply;
+    }
+
+    function updatePresalePrice(uint updatedPrice) public onlyOwner{
+        _preSalePrice = updatedPrice;
+    }
+
+    function findValueFromPercentage(
+        uint percentage,
+        uint totalAmount
+    ) private pure returns (uint) {
+        return (percentage * totalAmount) / 100;
+    }
+
+}
--- contracts/TERC20.sol
+++ contracts/TERC20.sol
@@ -12,62 +12,61 @@ contract TERC20 is ERC20, ERC20Burnable, Pausable, Ownable {
     uint public _maxSupply;
     uint public _supplyForPreSale;
 
-    constructor(string memory name, string memory symbol, address preSaleContract, uint maxSupply, uint percentageForPreSale) ERC20(name, symbol) {
+    constructor(
+        string memory name,
+        string memory symbol,
+        address preSaleContract,
+        uint maxSupply
+    ) ERC20(name, symbol) {
         updatePreSaleContract(preSaleContract);
         updateMaxSupply(maxSupply);
-        updateSupplyForPreSale(percentageForPreSale);
     }
 
-    modifier onlyAllowed(){   
+    modifier onlyAllowed() {
         require(msg.sender == owner() || msg.sender == _preSaleContract);
         _;
     }
 
     function mint(address to, uint256 amount) public onlyAllowed whenNotPaused {
-         uint amountWithDecimals = amount * (10 ** decimals());
+        uint amountWithDecimals = amount * (10 ** decimals());
 
-         maxSupplyIsNotReached(amountWithDecimals);
+        maxSupplyIsNotReached(amountWithDecimals);
 
         _mint(to, amountWithDecimals);
     }
-
-    // move presale token amt calculations to presale contract
-    // function mintFromPreSale(address to, uint256 amount) public onlyAllowed whenNotPaused {
-    //     uint amountWithDecimals = amount * (10 ** decimals());
-
-    //     maxSupplyIsNotReached(amountWithDecimals);
-
-    //     _mint(to, amountWithDecimals);
-    // }
 
     function forceMint(address to, uint256 amount) public onlyOwner {
-         uint amountWithDecimals = amount * (10 ** decimals());
+        uint amountWithDecimals = amount * (10 ** decimals());
         _mint(to, amountWithDecimals);
     }
 
-    function maxSupplyIsNotReached(uint tokenToMint) private view{
-        require(totalSupply() + tokenToMint <= _maxSupply * (10 ** decimals()), "ERC20: Max-Supply is reached");
+    function maxSupplyIsNotReached(uint tokenToMint) private view {
+        require(
+            totalSupply() + tokenToMint <= _maxSupply * (10 ** decimals()),
+            "ERC20: Max-Supply is reached"
+        );
     }
 
-    function findValueFromPercentage(uint percentage, uint totalAmount) private pure returns(uint){
+    function findValueFromPercentage(
+        uint percentage,
+        uint totalAmount
+    ) private pure returns (uint) {
         return (percentage * totalAmount) / 100;
     }
 
-    function updatePreSaleContract(address preSaleContract) onlyOwner public whenNotPaused{
-        require(preSaleContract.code.length > 0, "ERC20: Invalid pre-sale contract address");
+    function updatePreSaleContract(
+        address preSaleContract
+    ) public onlyOwner whenNotPaused {
+        require(
+            preSaleContract.code.length > 0,
+            "ERC20: Invalid pre-sale contract address"
+        );
         _preSaleContract = preSaleContract;
     }
 
-    function updateMaxSupply(uint maxSupply) onlyOwner public whenNotPaused{
+    function updateMaxSupply(uint maxSupply) public onlyOwner whenNotPaused {
         require(maxSupply > 0, "ERC20: Max-supply should be > 0");
         _maxSupply = maxSupply;
-    }
-
-    function updateSupplyForPreSale(uint percentageForPreSale) onlyOwner public whenNotPaused{
-        require(percentageForPreSale <= 100, "ERC20: Percentage for preSale should be <= 100");
-        
-        uint totalPreSaleTokens = findValueFromPercentage(percentageForPreSale, _maxSupply);
-        _supplyForPreSale = totalPreSaleTokens;
     }
 
     function pause() public onlyOwner {
@@ -78,11 +77,11 @@ contract TERC20 is ERC20, ERC20Burnable, Pausable, Ownable {
         _unpause();
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
